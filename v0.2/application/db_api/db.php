ba10abc987ef9a6cad278d1ff53e668c50b3cf9c
CodeCrafterNixx <3878575267@qq.com>
2025-05-03 09:54:45 +0800

    Update db.php

--- v0.2/application/db_api/db.php
+++ v0.2/application/db_api/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "jm65zzt";
-$password = "ufhk0qt";
-$dbname = "jm65zzt";
+$servername = "你的MySQL主机地址";
+$username = "你的MySQL用户名";
+$password = "你的MySQL密码";
+$dbname = "你的MySQL数据库名";
 
 // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
